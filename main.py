7ed6c5a508c6373f72af6ea3db6010d1309f774b
economyaws <economyaws@gmail.com>
2024-12-03 12:45:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_headers=["*"],  # Permite todos os cabeçalhos
 )
 
-token_id = 'mz71n43h67yp3fni'  # Seu token de acesso    
+token_id = 'mz7'  # Seu token de acesso    
 host = 'economyenergy.bitrix24.com.br'  # O host do seu Bitrix24
 user = 1  # ID do usuário
 
@@ -26,19 +26,15 @@ async def fetch(session, url, params):
         response.raise_for_status()
         return await response.json()
 
-# Função para obter os leads com ID > last_id e start=-1
-async def get_leads(session, email_filter, last_id=None):
+async def get_leads(session, email_filter):
     url = f'https://{host}/rest/{user}/{token_id}/crm.lead.list/'
     params = {
-        'order': {'ID': 'ASC'},  # Ordena os leads por ID (crescente)
+        'order': {'DATE_CREATE': 'DESC'},
         'filter': {'UF_CRM_1717008267006': email_filter},
         'select': ['ID', 'OPPORTUNITY', 'STATUS_ID', 'UF_CRM_1717008267006', 'TITLE', 'DATE_CREATE', 'CONTACT_ID', 'PHONE', 'UF_CRM_1717008267006', 'UF_CRM_1716238809742', 'UF_CRM_1721931621996'],
-        'start': -1,  # Desabilita o contador de elementos
-        'limit': 50  # Define o limite por página
+        'start': 0,
+        'limit': 50
     }
-
-    if last_id:
-        params['filter']['ID'] = {'>': last_id}  # Filtra para IDs maiores que o último ID recebido
 
     all_leads = []
     while True:
@@ -47,28 +43,22 @@ async def get_leads(session, email_filter, last_id=None):
             all_leads.extend(response['result'])
         
         # Verificar se há mais resultados para buscar
-        if len(response.get('result', [])) < params['limit']:
+        if 'next' in response:
+            params['start'] = response['next']
+        else:
             break
-        
-        # Atualiza o last_id com o ID do último item recebido
-        last_id = response['result'][-1]['ID']
-        params['filter']['ID'] = {'>': last_id}  # Define a condição de filtro para IDs maiores
+    
+    return {'result': all_leads}
 
-    return all_leads
-
-# Função para obter os deals com ID > last_id e start=-1
-async def get_deals(session, email_filter, last_id=None):
+async def get_deals(session, email_filter):
     url = f'https://{host}/rest/{user}/{token_id}/crm.deal.list/'
     params = {
-        'order': {'ID': 'ASC'},  # Ordena os deals por ID (crescente)
+        'order': {'DATE_CREATE': 'DESC'},
         'filter': {'UF_CRM_6657792586A0F': email_filter},
         'select': ['PHONE', 'CATEGORY_ID', 'OPPORTUNITY', 'STAGE_ID', 'UF_CRM_6657792586A0F', 'TITLE', 'DATE_CREATE', 'CONTACT_ID', 'UF_CRM_1716235306165', 'UF_CRM_1709207938786', 'UF_CRM_664BBA75E0765', 'UF_CRM_1709060681601', 'UF_CRM_1716236663328', 'UF_CRM_1716235986482'],
-        'start': -1,  # Desabilita o contador de elementos
-        'limit': 50  # Define o limite por página
+        'start': 0,
+        'limit': 50
     }
-
-    if last_id:
-        params['filter']['ID'] = {'>': last_id}  # Filtra para IDs maiores que o último ID recebido
 
     all_deals = []
     while True:
@@ -77,16 +67,13 @@ async def get_deals(session, email_filter, last_id=None):
             all_deals.extend(response['result'])
         
         # Verificar se há mais resultados para buscar
-        if len(response.get('result', [])) < params['limit']:
+        if 'next' in response:
+            params['start'] = response['next']
+        else:
             break
-        
-        # Atualiza o last_id com o ID do último item recebido
-        last_id = response['result'][-1]['ID']
-        params['filter']['ID'] = {'>': last_id}  # Define a condição de filtro para IDs maiores
+    
+    return {'result': all_deals}
 
-    return all_deals
-
-# Função para obter o telefone do contato
 async def get_contact_phone(session, contact_id):
     if not contact_id:
         return None
@@ -95,25 +82,25 @@ async def get_contact_phone(session, contact_id):
     response = await fetch(session, url, params)
     return response.get('result', {}).get('PHONE', [])
 
-# Função para processar os dados simultaneamente
 async def get_data(session, email_filter):
-    leads = await get_leads(session, email_filter)
-    deals = await get_deals(session, email_filter)
-    return leads, deals
+    leads_task = get_leads(session, email_filter)
+    deals_task = get_deals(session, email_filter)
+    return await asyncio.gather(leads_task, deals_task)
 
 @app.post("/get_data")
 async def get_data_endpoint(request: EmailRequest):
     async with aiohttp.ClientSession() as session:
         email_filter = request.email
-
+        
         # Obter leads e deals simultaneamente
         leads, deals = await get_data(session, email_filter)
 
         all_data = []
 
         # Processar leads
-        if leads:
-            for lead in leads:
+        if leads and 'result' in leads:
+            result_leads = leads['result']
+            for lead in result_leads:
                 if 'PHONE' in lead and isinstance(lead['PHONE'], list) and len(lead['PHONE']) > 0:
                     lead['PHONE'] = lead['PHONE'][0]['VALUE']
                 else:
@@ -123,11 +110,12 @@ async def get_data_endpoint(request: EmailRequest):
             raise HTTPException(status_code=404, detail="Leads not found")
 
         # Processar deals
-        if deals:
-            tasks = [get_contact_phone(session, deal.get('CONTACT_ID')) for deal in deals]
+        if deals and 'result' in deals:
+            result_deals = deals['result']
+            tasks = [get_contact_phone(session, deal.get('CONTACT_ID')) for deal in result_deals]
             phones = await asyncio.gather(*tasks)
 
-            for deal, phone in zip(deals, phones):
+            for deal, phone in zip(result_deals, phones):
                 if phone and len(phone) > 0:
                     deal['PHONE'] = phone[0]['VALUE']
                 else:
